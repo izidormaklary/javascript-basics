7d154fccc53bf51cb1e7734ea9d70d1926e3644e
izidormaklary <maklaryizidor@gmail.com>
2021-05-03 14:59:55 +0200

    done

--- js/js-complete-course/06-dom/12-mini-carousel-Optional/script.js
+++ js/js-complete-course/06-dom/12-mini-carousel-Optional/script.js
@@ -18,7 +18,16 @@
         "../../_shared/img/lemon.svg",
         "../../_shared/img/map.svg",
     ];
+const next = document.getElementById("next");
+const image = document.getElementsByTagName("IMG")[0];
+    let i = 0;
+    next.addEventListener("click", ev => {
 
-    // your code here
+        ++i;
+        image.src = gallery[i];
+        if (i === gallery.length-1){
+            i = 0;
+        }
+    } )
 
 })();
