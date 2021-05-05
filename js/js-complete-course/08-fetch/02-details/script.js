ccfa45c1d6701690fa00b72fb97ce3c6b33907f4
Veronika Breuer <veronikabreuer@Nonis-MacBook.local>
2021-05-05 10:43:13 +0200

    done

--- js/js-complete-course/08-fetch/02-details/script.js
+++ js/js-complete-course/08-fetch/02-details/script.js
@@ -10,5 +10,40 @@
 // You will have time to focus on it later.
 
 (() => {
+    
+    const hName = document.getElementsByClassName('name');
+    document.getElementById("run").addEventListener("click", getXmen);
+    
+    async function getXmen() {
+        let heroId = document.getElementById('hero-id').value;
+        let data = await fetch(`http://localhost:3000/heroes?id=${heroId}`);
+        let heroObj = await data.json();
+        heroObj = heroObj[0];
+        createTemplate(heroObj)
+          
+    }
+
+    function createTemplate(heroObj){
+        const target = document.getElementById("target");
+        template = document.getElementById('tpl-hero').content.cloneNode(true);
+        const name = template.querySelector(".name");
+        const alt = template.querySelector(".alter-ego");
+        let abilities =  template.querySelector(".powers");
+        target.appendChild(template);
+        name.innetText= heroObj.name;
+        alt.innerText= heroObj.alterEgo;
+        
+       
+        heroObj.abilities.forEach(element => {
+          
+           let br = document.createElement("br");
+           let el = document.createTextNode(element);
+           
+           abilities.appendChild(el);
+           abilities.appendChild(br);
+        })
+        
+    }
+
     // your code here
 })();
